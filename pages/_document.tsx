3e375a455500dc4bb7a6e48863f421deff43bb58
akerman <akerman@akerman>
2022-04-26 18:56:24 -0300

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initalProps = await Document.getInitialProps(ctx)
+
+        return initalProps
+    }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script async src="https://sdk.mercadopago.com/js/v2"></script>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
